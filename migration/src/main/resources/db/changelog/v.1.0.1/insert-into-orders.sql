a385879d87ab4ee8f7e035c3c934304ca12023d6
Maxim447 <maximbas777@gmail.com>
2023-10-17 13:52:22 +0300

    fixed bugs in the insert-into-orders sql script

--- migration/src/main/resources/db/changelog/v.1.0.1/insert-into-orders.sql
+++ migration/src/main/resources/db/changelog/v.1.0.1/insert-into-orders.sql
@@ -1,5 +1,5 @@
 INSERT INTO orders (id, customer_id, restaurant_id, status, courier_id, timestamp)
 VALUES
-    (1, 1, 1, 'active', 1, localtimestamp),
-    (2, 2, 2, 'delivered', 2, localtimestamp)
+    (1, 1, 1, 'active', 1, now()),
+    (2, 2, 2, 'delivered', 2, now())
 ;
