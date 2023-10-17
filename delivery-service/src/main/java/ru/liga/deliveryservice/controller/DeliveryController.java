5661ba03a072e9e93071534b5b6d1255eba79e6e
Maxim447 <maximbas777@gmail.com>
2023-10-17 13:17:32 +0300

    fixed logical errors in the delivery service

--- delivery-service/src/main/java/ru/liga/deliveryservice/controller/DeliveryController.java
+++ delivery-service/src/main/java/ru/liga/deliveryservice/controller/DeliveryController.java
@@ -4,6 +4,7 @@ package ru.liga.deliveryservice.controller;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,16 +14,12 @@ import ru.liga.deliveryservice.service.DeliveryService;
 
 @Tag(name = "API для отправки заказов курьерам")
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/")
 public class DeliveryController {
 
     @Schema(description = "Сервис для отправки заказов курьерам")
     private final DeliveryService deliveryService;
-
-    @Autowired
-    public DeliveryController(DeliveryService deliveryService) {
-        this.deliveryService = deliveryService;
-    }
 
     @Operation(summary = "Получить все доставки")
     @GetMapping("/deliveries")
