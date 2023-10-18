a6469297b2865cc7cb7671fb0589235a8fd37f84
Maxim447 <maximbas777@gmail.com>
2023-10-18 12:19:20 +0300

    added module common

---
+++ common/src/main/java/ru/liga/dto/GetKitchenOrdersResponseDTO.java
@@ -0,0 +1,24 @@
+package ru.liga.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+public class GetKitchenOrdersResponseDTO {
+
+    @Schema(description = "Заказы")
+    private List<KitchenOrderDTO> orders;
+
+    @Schema(description = "Индекс страницы")
+    @JsonProperty("page_index")
+    private Integer pageIndex;
+
+    @Schema(description = "Счетчик страницы")
+    @JsonProperty("page_count")
+    private Integer pageCount;
+}
--- kitchen-service/src/main/java/ru/liga/kitchenservice/service/KitchenService.java
+++ kitchen-service/src/main/java/ru/liga/kitchenservice/service/KitchenService.java
@@ -3,7 +3,7 @@ package ru.liga.kitchenservice.service;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Schema;
 import org.springframework.stereotype.Service;
-import ru.liga.dto.GetOrdersResponseDTO;
+import ru.liga.dto.GetKitchenOrdersResponseDTO;
 import ru.liga.dto.MenuItemsDTO;
 import ru.liga.dto.KitchenOrderDTO;
 
@@ -14,8 +14,8 @@ import java.util.List;
 public class KitchenService {
 
     @Operation(summary = "Получить все заказы")
-    public GetOrdersResponseDTO getOrders(String status) {
-        return new GetOrdersResponseDTO(List.of(
+    public GetKitchenOrdersResponseDTO getOrders(String status) {
+        return new GetKitchenOrdersResponseDTO(List.of(
                 new KitchenOrderDTO(1L, List.of(new MenuItemsDTO(10, 1))),
                 new KitchenOrderDTO(2L, List.of(new MenuItemsDTO(1, 2)))),
                 1,
