258f6c52a2026cb99a17ed04f705cb24afd4718b
Maxim447 <maximbas777@gmail.com>
2023-10-19 12:48:11 +0300

    changed insert values

--- migration/src/main/resources/db/changelog/v.1.0.1/insert-into-customers.sql
+++ migration/src/main/resources/db/changelog/v.1.0.1/insert-into-customers.sql
@@ -1,5 +1,14 @@
 INSERT INTO customers (id, phone, email, address)
 VALUES
-    (1, '+7-999-999-99-99', 'someemail@gmail.com', 'Nizhniy Novgorod, Vaneeva st., 57'),
-    (2, '+7-900-000-00-00', 'test@test.com', 'Nizhniy Novgorod, Big Pokrovskaya st., 21')
-;
+    (1, '+7-999-999-99-99', 'someemail@gmail.com', 'Vaneeva 57'),
+    (2, '+7-900-000-00-00', 'test@test.com', 'Big Pokrovskaya 21'),
+    (3, '+7-908-333-12-66', 'random@gmail.ru', 'Lvovskaya 3'),
+    (4, '+7-950-111-56-22', 'random2@mail.ru', 'Rodionova 171'),
+    (5, '+7-902-777-89-88', 'random3@ya.ru', 'Kostina 23'),
+    (6, '+7-988-333-12-66', 'random4@gmail.ru', 'Lvovskaya 4'),
+    (7, '+7-951-111-56-22', 'random5@mail.ru', 'Rodionova 5'),
+    (8, '+7-903-777-89-88', 'random6@ya.ru', 'Kostina 7'),
+    (9, '+7-907-333-12-66', 'random7@gmail.ru', 'Lvovskaya 1'),
+    (10, '+7-954-111-56-22', 'random8@mail.ru', 'Rodionova 43'),
+    (11, '+7-910-777-89-88', 'random9@ya.ru', 'Kostina 81'),
+    (12, '+7-911-777-89-88', 'random10@ya.ru', 'Kostina 11');
