4da8e9fcc72b5043cee58708f0bc9e7ccd19bc67
Maxim447 <maximbas777@gmail.com>
2023-10-22 22:37:07 +0300

    database updated

---
+++ migration/src/main/resources/db/changelog/v.1.0.4/update-value-in-customers-table.sql
@@ -0,0 +1,47 @@
+UPDATE customers
+set longitude = 56.315766781368325, latitude = 44.015769828835325
+where id = 1;
+
+UPDATE customers
+set longitude = 56.321820416036786, latitude = 43.99976005492841
+where id = 2;
+
+UPDATE customers
+set longitude = 56.2679449614115, latitude = 43.87525442239521
+where id = 3;
+
+UPDATE customers
+set longitude = 56.320816922894515, latitude = 44.04385387822173
+where id = 4;
+
+UPDATE customers
+set longitude = 56.30965387862878, latitude = 43.99093383958632
+where id = 5;
+
+UPDATE customers
+set longitude = 56.268207522133615, latitude = 43.8818065745239
+where id = 6;
+
+UPDATE customers
+set longitude = 56.320112421819864, latitude = 44.04601184753906
+where id = 7;
+
+UPDATE customers
+set longitude = 56.31094828919684, latitude = 43.990902268421685
+where id = 8;
+
+UPDATE customers
+set longitude = 56.26738065248304, latitude = 43.87412869355982
+where id = 9;
+
+UPDATE customers
+set longitude = 56.31561912695005, latitude = 44.06297103404477
+where id = 10;
+
+UPDATE customers
+set longitude = 56.30970744056807, latitude = 43.99092311075099
+where id = 11;
+
+UPDATE customers
+set longitude = 56.31004428604421, latitude = 43.99100329540975
+where id = 12;
---
+++ migration/src/main/resources/db/changelog/v.1.0.4/update-value-in-restaurants-table.sql
@@ -0,0 +1,19 @@
+UPDATE restaurants
+SET longitude = 56.326188712131206, latitude = 44.02524682609331
+where id = 1;
+
+UPDATE restaurants
+SET longitude = 56.34044393176929, latitude = 43.91494601075289
+where id = 2;
+
+UPDATE restaurants
+SET longitude = 56.31172662229177, latitude = 43.98061489356252
+where id = 3;
+
+UPDATE restaurants
+SET longitude = 56.32539386395687, latitude = 44.00411575187746
+where id = 4;
+
+UPDATE restaurants
+SET longitude = 56.33024297330632, latitude = 43.99616686842285
+where id = 5;
