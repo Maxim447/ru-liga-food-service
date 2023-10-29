86e8a036510a04a85e1ed004317d4cfcf5bafef2
Maxim447 <maximbas777@gmail.com>
2023-10-29 17:47:31 +0300

    updated courier search logic

---
+++ migration/src/main/resources/db/changelog/v.1.0.6/update-couriers-table.sql
@@ -0,0 +1,11 @@
+UPDATE couriers
+set longitude = 56.32535323974714, latitude = 44.01063399530111
+where id = 1;
+
+UPDATE couriers
+set longitude = 56.309428272664945, latitude = 43.93478415939542
+where id = 2;
+
+UPDATE couriers
+set longitude = 56.336734074707074, latitude = 43.914403665935275
+where id = 3;
